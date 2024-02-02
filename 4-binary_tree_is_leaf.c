236f57f4bcce385dbc5bb69dfb72f87924c7ae13
otienojoseph <otienoj.cruz@gmail.com>
2024-02-02 23:02:50 +0300

    Add binary_tree_is_leaf function Function that returns 1 if node is a leaf 0 otherwise

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Function that checks if node is a leaf
+ * @node: pointer to node to check
+ * Return: 1 if leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	binary_tree_is_leaf(node->left);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (binary_tree_is_leaf(node->left) && binary_tree_is_leaf(node->right));
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 - 1-binary_tree_insert_left - Inserts node as the left-child of another node
 - 2-binary_tree_insert_right - Inserts node as the right-child of another node
 - 3-binary_tree_delete - Delete an entire binary tree
+- 4-binary_tree_is_leaf - Function that checks if node is a leaf
