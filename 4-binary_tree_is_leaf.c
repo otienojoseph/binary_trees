2243f96af5b345fd11169ef7376e5b43e58e62fc
otienojoseph <otienoj.cruz@gmail.com>
2024-02-02 23:49:24 +0300

    feat: Check root that has one right and left node

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -12,5 +12,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 
 	if (node->left == NULL && node->right == NULL)
 		return (1);
-	return (binary_tree_is_leaf(node->left) && binary_tree_is_leaf(node->right));
+	if (node->left != NULL && node->right != NULL)
+		return (binary_tree_is_leaf(node->left) && binary_tree_is_leaf(node->right));
 }
