b346c6d18c403349e945dbf8d40392e6c66b8011
otienojoseph <otienoj.cruz@gmail.com>
2024-02-02 23:05:50 +0300

    fix: Remove unnecessary peice of code

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,7 +10,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	binary_tree_is_leaf(node->left);
 	if (node->left == NULL && node->right == NULL)
 		return (1);
 	return (binary_tree_is_leaf(node->left) && binary_tree_is_leaf(node->right));
