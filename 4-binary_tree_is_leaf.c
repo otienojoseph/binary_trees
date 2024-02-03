76de3f8ed85d7d3e21b3d391aebf6f91bc749c31
otienojoseph <otienoj.cruz@gmail.com>
2024-02-03 23:03:48 +0300

    fix: Add passing check if node is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -12,7 +12,9 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 
 	if (node->left == NULL && node->right == NULL)
 		return (1);
+
 	if (node->left != NULL && node->right != NULL)
-		return (binary_tree_is_leaf(node->left) && binary_tree_is_leaf(node->right));
+		return (0);
+
 	return (0);
 }
