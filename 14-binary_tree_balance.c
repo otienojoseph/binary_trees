8291daac01b4924687576a1a8c73211ac8737c4f
otienojoseph <otienoj.cruz@gmail.com>
2024-02-07 00:39:25 +0300

    fix: Add betty compliance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,12 +26,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	 if (tree == NULL)
-		 return (0);
+	if (tree == NULL)
+		return (0);
 
-	 left_height = binary_tree_height(tree->left) + 1;
-	 right_height = binary_tree_height(tree->right) + 1;
+	left_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
 
-	 return ((left_height > right_height) ? (left_height) :
-		 (right_height));
+	return ((left_height > right_height) ? (left_height) :
+			(right_height));
 }
