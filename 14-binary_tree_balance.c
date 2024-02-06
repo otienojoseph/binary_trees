e86a23645487c8cc7be25a335ed8c7c18655053f
otienojoseph <otienoj.cruz@gmail.com>
2024-02-07 00:24:54 +0300

    fix: Remove whitepace

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,5 +14,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return (left_height - right_height); 
+	return (left_height - right_height);
 }
